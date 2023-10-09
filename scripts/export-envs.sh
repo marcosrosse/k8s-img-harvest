15cf131c824537d301bde9a11f4c2c6286cd329f
marcosrosse <marcosrossem@gmail.com>
2023-10-09 17:54:54 +0100

    update export script

--- scripts/export-envs.sh
+++ scripts/export-envs.sh
@@ -1,4 +1,5 @@
-echo export K8S_CA_CERT="/home/mrossemo/.minikube/ca.crt"
-echo export K8S_CERT="/home/mrossemo/.minikube/profiles/minikube/client.crt"
-echo export K8S_KEY_CERT="/home/mrossemo/.minikube/profiles/minikube/client.key"
-echo export K8S_ADDRESS="https://192.168.39.44:8443"
+echo export K8S_CA_CERT="$HOME/.minikube/ca.crt"
+echo export K8S_CERT="$HOME/.minikube/profiles/minikube/client.crt"
+echo export K8S_KEY_CERT="$HOME/.minikube/profiles/minikube/client.key"
+echo export K8S_ADDRESS=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
+echo export K8S_JWT_TOKEN=$(kubectl -n kube-system create token default)
