37a1a333b1c3046ab47569e7b30c80e465b0a396
marcosrosse <marcosrossem@gmail.com>
2023-10-09 17:55:58 +0100

    implement jwt token in the request

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"io"
 	"log"
 	"net/http"
 	"os"
 )
+
 var (
 	k8s_addr = os.Getenv("K8S_ADDRESS")
-	bearer = os.Getenv("K8S_JWT_TOKEN")
+	jwt      = os.Getenv("K8S_JWT_TOKEN")
 )
 
-func main() {
-	resp, err := http.Get(k8s_addr+"/apis/apps/v1/deployments")
-	resp.Header.Set("Authorization",bearer)
-	resp.Header.Add("Accept", "application/json")
+func Request(method, path string, body io.Reader) (*http.Request, error) {
+	bearer := "Bearer " + jwt
+
+	// Creating new request
+	r, err := http.NewRequest(method, path, body)
+	r.Header.Set("Authorization", bearer)
+	r.Header.Add("Accept", "application/json")
 	if err != nil {
-		log.Println(err)
+		log.Println("Error creating request: ", err)
+		return nil, err
 	}
+	return r, nil
+
+}
+
+func main() {
+	podsPath := "/api/v1/pods"
+	url := k8s_addr + podsPath
+
+	req, err := Request("GET", url, bytes.NewBuffer(nil))
+	if err != nil {
+		log.Println("Error creating request: ", err)
+	}
+
+	// // Seding request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+
+	// Closing request
 	defer resp.Body.Close()
 
-	msg, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(msg))
+	// Reading th request body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error while reading the response bytes:", err)
+	}
+	log.Println(string([]byte(body)))
 }
